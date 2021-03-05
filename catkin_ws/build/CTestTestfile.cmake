3e55ff710d99d12cc9b87be254b812d4fd257ae9
jay-95 <dnjswo0205@gmail.com>
2021-03-05 11:09:54 +0900

    ros_tutorial_updated_2021-03-05

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,19 @@
+# CMake generated Testfile for 
+# Source directory: /home/lee/catkin_ws/src
+# Build directory: /home/lee/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3_msgs")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("basics")
+subdirs("my_first_ros_pkg")
+subdirs("my_package")
+subdirs("ros_tutorials_topic")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3/turtlebot3_example")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("turtlebot3/turtlebot3_description")
---
+++ catkin_ws/build/turtlebot3/turtlebot3_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lee/catkin_ws/src/turtlebot3/turtlebot3_bringup
+# Build directory: /home/lee/catkin_ws/build/turtlebot3/turtlebot3_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
